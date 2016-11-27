80ddf4c20b9b88ff2ff25345af2cc5b1156d2904
Jubins <jubins@nyu.edu>
2016-11-27 15:09:49 -0500

    computervision code
    computer vision code

---
+++ computervision.py
@@ -0,0 +1,27 @@
+########### Python 2.7 #############
+import httplib, urllib, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '{subscription key}',
+}
+
+params = urllib.urlencode({
+    # Request parameters
+    'visualFeatures': 'Categories',
+    'details': '{string}',
+    'language': 'en',
+})
+
+try:
+    conn = httplib.HTTPSConnection('api.projectoxford.ai')
+    conn.request("POST", "/vision/v1.0/analyze?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
